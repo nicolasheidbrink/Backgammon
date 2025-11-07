e4b3fb4c492ae9a8e72e270f15dbfbf90f9a2469
nicol <nicol@10.204.135.32>
2025-11-07 19:12:59 +0100

    tester, blot evaluation

--- Backgammon/src/main/java/backgammon/model/engines/ruleBased/PositionEvaluator.java
+++ Backgammon/src/main/java/backgammon/model/engines/ruleBased/PositionEvaluator.java
@@ -1,8 +1,12 @@
 package backgammon.model.engines.ruleBased;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import backgammon.model.gameCalculations.GameCalculation;
 import backgammon.model.gameModels.Board;
 import backgammon.model.gameModels.CheckerColors;
+import backgammon.model.gameModels.Point;
 
 public class PositionEvaluator {
 
@@ -13,9 +17,29 @@ public class PositionEvaluator {
 		runningTotal += pipDifference;
 		
 		
-		double barPenalty = (2.0 + 4.0 * GameCalculation.calculateAmountOfTowersInHome(board, CheckerColors.O)) * board.barX
-						- (2.0 + 4.0 * GameCalculation.calculateAmountOfTowersInHome(board, CheckerColors.X)) * board.barO;
+		double barPenalty = (2.0 + 3.0 * GameCalculation.calculateAmountOfTowersInHome(board, CheckerColors.O)) * board.barX
+						- (2.0 + 3.0 * GameCalculation.calculateAmountOfTowersInHome(board, CheckerColors.X)) * board.barO;
 		runningTotal += barPenalty;
+		
+		double blotPenalty = 0;
+		for(int i = 0; i < 24; i++){
+			if(board.points[i].amtCheckers == 1){
+				double hittable = 0;
+				try{
+					for(int j = i + board.points[i].occupiedBy.direction; Math.abs(j-i) <= 6; j += board.points[i].occupiedBy.direction){
+						try{
+							if((j == board.points[i].occupiedBy.opposite.barInt && board.getBar(board.points[i].occupiedBy.opposite) > 0) 
+									||  board.points[j].occupiedBy == board.points[i].occupiedBy.opposite){
+								hittable += rollProbabilities.get(Math.abs(j-i));
+							}
+						} catch(Exception e){ break; }
+					}
+				} catch(Exception e){}
+				blotPenalty += board.points[i].occupiedBy.direction * 2 
+							+ hittable * board.points[i].occupiedBy.direction * (3 + Math.abs(i - board.points[i].occupiedBy.barInt) * 0.35) ;
+			}
+		}
+		runningTotal += blotPenalty;
 		
 		double rewardTowers = 0.0;
 		for(int i = 0; i < 24; i++){
@@ -31,4 +55,32 @@ public class PositionEvaluator {
 		return runningTotal;
 	}
 	private static int[] towerValues = new int[]{6, 7, 8, 9, 10, 11,	8, 7, 6, 5, 5, 5,	4, 4, 4, 4, 4, 4,	2, 2, 2, 2, 2, 1};
+	
+	private static Map<Integer, Double> rollProbabilities = new HashMap<>();
+	static{
+		rollProbabilities.put(1, 11.0/36.0);
+		rollProbabilities.put(2, 12.0/36.0);
+		rollProbabilities.put(3, 14.0/36.0);
+		rollProbabilities.put(4, 15.0/36.0);
+		rollProbabilities.put(5, 15.0/36.0);
+		rollProbabilities.put(6, 17.0/36.0);
+		rollProbabilities.put(7, 6.0/36.0);
+		rollProbabilities.put(8, 6.0/36.0);
+		rollProbabilities.put(9, 5.0/36.0);
+		rollProbabilities.put(10, 3.0/36.0);
+		rollProbabilities.put(11, 2.0/36.0);
+		rollProbabilities.put(12, 3.0/36.0);
+		rollProbabilities.put(13, 0.0/36.0);
+		rollProbabilities.put(14, 0.0/36.0);
+		rollProbabilities.put(15, 1.0/36.0);
+		rollProbabilities.put(16, 1.0/36.0);
+		rollProbabilities.put(17, 0.0/36.0);
+		rollProbabilities.put(18, 1.0/36.0);
+		rollProbabilities.put(19, 0.0/36.0);
+		rollProbabilities.put(20, 1.0/36.0);
+		rollProbabilities.put(21, 0.0/36.0);
+		rollProbabilities.put(22, 0.0/36.0);
+		rollProbabilities.put(23, 0.0/36.0);
+		rollProbabilities.put(24, 1.0/36.0);
+	}
 }
--- Backgammon/src/main/java/backgammon/model/selfPlay/SelfPlayer.java
+++ Backgammon/src/main/java/backgammon/model/selfPlay/SelfPlayer.java
@@ -3,9 +3,14 @@ package backgammon.model.selfPlay;
 import backgammon.model.engines.Engine;
 import backgammon.model.engines.randomMove.RandomMoveEngine;
 import backgammon.model.engines.ruleBased.RuleBasedEngine;
+import backgammon.model.engines.tester.RuleBasedEngineTester;
 import backgammon.model.gameCalculations.GameCalculation;
 import backgammon.model.gameModels.Board;
 import backgammon.model.gameModels.CheckerColors;
+
+import java.util.Arrays;
+
+import org.apache.commons.math3.distribution.NormalDistribution;
 
 public class SelfPlayer {
 
@@ -15,19 +20,47 @@ public class SelfPlayer {
 	int scoreO = 0;
 	int scoreX = 0;
 	
+	int n = 1000;
+	int[] results = new int[1000];
+	int i = 0;
+	
 	Board board;
 	CheckerColors currentTurn;
 	
 	public SelfPlayer(){
 		engineO = new RuleBasedEngine();
-		engineX = new RuleBasedEngine();
+		engineX = new RuleBasedEngineTester();
 		
 		this.board = new Board();
 		currentTurn = CheckerColors.O;
-		for(int i = 0; i < 1000; i++){
+		for(int i = 0; i < n; i++){
 			System.out.println("\n\n\n\ni: "+i+"\n");
 			playTurn();
 		}
+		double X_n = ((double) (scoreO - scoreX)) / n;
+		double s = Math.sqrt(
+						Arrays.stream(results)
+								.asDoubleStream()
+								.map(x -> Math.pow(x - X_n, 2))
+								.sum() / (n-1)
+					);
+		double avgPointsPerGame = (double) (scoreO + scoreX) / n;
+		
+		int numberOfOVictories = (int) Arrays.stream(results)
+									.filter(x -> x > 0)
+									.count();
+ 
+		double t = (X_n - 0) / (s/Math.sqrt(n));
+				
+		double pValue = new NormalDistribution(0, 1).cumulativeProbability(t);
+		System.out.println("After "+n+" games, EngineO won "+scoreO+" points and EngineX won "+scoreX+" points"+
+				"\nThe amount of games won by O is "+numberOfOVictories+
+				"\n\nThe average score per game is "+avgPointsPerGame+
+				"\nThe average score per game that O won is "+(double) scoreO / numberOfOVictories+
+				"\nThe average score per game that X won is "+(double) scoreX / (n-numberOfOVictories)+
+				"\n\nThe mean score is "+X_n+
+				"\nThe t Value is "+t+
+				"\nThe p Value of O being better than X is "+pValue);
 	}
 	
 	public void playTurn(){
@@ -39,6 +72,7 @@ public class SelfPlayer {
 			if(board.getTray(currentTurn) == 15){
 				int winFactor = GameCalculation.calculateWinFactor(board, CheckerColors.O);
 				scoreO += winFactor;
+				results[i++] = winFactor;
 				System.out.println("\n*****O WINS WITH "+winFactor+"*****\n");
 				System.out.println("ScoreO: "+scoreO+"\nScoreX: "+scoreX + "\n\n\n");
 				board = new Board();
@@ -52,7 +86,8 @@ public class SelfPlayer {
 			if(board.getTray(currentTurn) == 15){
 				int winFactor = GameCalculation.calculateWinFactor(board, CheckerColors.X);
 				scoreX += winFactor;
-				System.out.println("\n\n\n\n\n*****X WINS WITH "+winFactor+"*****\n");
+				results[i++] = - winFactor;
+				System.out.println("\n\n*****X WINS WITH "+winFactor+"*****\n");
 				System.out.println("ScoreO: "+scoreO+"\nScoreX: "+scoreX + "\n\n\n");
 				board = new Board();
 				return;
